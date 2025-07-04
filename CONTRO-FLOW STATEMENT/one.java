d980dfee3811bc279bd588c0a8c6350153a12727
bimalchunara2064 <bimalchunara29@gmail.com>
2025-07-04 11:46:05 +0545

    JAVA FULL COURSE

---
+++ CONTRO-FLOW STATEMENT/one.java
@@ -0,0 +1,8 @@
+public class one{
+public static void main(String[]args){
+int age = 20;
+if (age >= 18) {
+    System.out.println("I am Bimal Chunara");
+}
+}
+}
---
+++ CONTRO-FLOW STATEMENT/three.java
@@ -0,0 +1,12 @@
+public class three{
+public static void main(String[]args){
+int marks = 75;
+if (marks >= 90) {
+    System.out.println("Grade A");
+} else if (marks >= 70) {
+    System.out.println("Grade B");
+} else {
+    System.out.println("Grade C");
+}
+}
+}
